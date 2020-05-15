258d2f0e05218f9d64a532c998af364379c57e52
kxpedro <pedrorshenrique1515@hotmail.com>
2020-05-14 22:46:54 -0300

    AppUser

---
+++ WebService/WebService/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int AppUserID { get; set; }
+        public string AppUsername { get; set; }
+        public string AppPassword { get; set; }
+
+    }
+}
